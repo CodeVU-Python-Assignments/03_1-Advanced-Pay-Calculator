6f19a320647e0253eb8b3fa3d8965d67257ad49a
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2023-01-30 09:43:57 -0600

    Update test_optional.py

--- test_optional.py
+++ test_optional.py
@@ -5,7 +5,10 @@ def test_payCalculator_prints_error_withNonNumericHoursBonus(capfd, monkeypatch)
     hours = 'njdvnjkfdfdb'
     input = [10, hours]
     monkeypatch.setattr('builtins.input', lambda _:input.pop())
-    payCalculator.calculatePay()
+    try:
+        payCalculator.calculatePay()
+    except:
+        pass
 
     out, err = capfd.readouterr()
     expected = 'Error, please enter numeric input\n'
@@ -15,7 +18,10 @@ def test_payCalculator_prints_error_withNonNumericPayBonus(capfd, monkeypatch):
     hours = randint(1, 100)
     input = [rate, hours]
     monkeypatch.setattr('builtins.input', lambda _:input.pop())
-    payCalculator.calculatePay()
+    try:
+        payCalculator.calculatePay()
+    except:
+        pass
 
     out, err = capfd.readouterr()
     expected = 'Error, please enter numeric input\n'
